5864d8320c00c334f33138ce9a0f7c6253441abb
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-03 08:22:27 -0700

    added 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,10 +17,10 @@ int main(void)
 	/* your code goes there */
 	for (num1 = 0 ; num1 < 9 ; num1++)
 	{
-	for (num2 = num1 + 1 ; num2 <=9 ; num2++)
+	for (num2 = num1 + 1 ; num2 < 10 ; num2++)
 	{
-	putchar(num1 + '0');
-	putchar(num2 + '0');
+	putchar((num1 % 10) + '0');
+	putchar((num2 % 10) + '0');
 	if (num1 != 8 && num2 != 9)
 	{
 	putchar(',');
