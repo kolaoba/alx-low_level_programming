4ca0a7701f632d239d31835c502d4eb121f43427
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-03 12:10:44 -0700

    fixed 102-print_comb5 finally

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,7 +21,7 @@ int main(void)
 	{
 	putchar((num1 / 10) + '0');
 	putchar((num2 % 10) + '0');
-	putchar(' ')
+	putchar(' ');
 	putchar((num2 / 10) + '0');
 	putchar((num2 % 10) + '0');
 	if (num1 == 98 && num2 == 99)
