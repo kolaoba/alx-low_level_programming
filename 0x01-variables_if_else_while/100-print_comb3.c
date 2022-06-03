d28ab35a9a0d4bc9da22b294bd57678990e7a3b7
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-03 08:19:06 -0700

    added 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - program prints all possible combination of two digits
+ *
+ * Description - program prints all possible combination of two digits
+ * Return: Always 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	/* your code goes there */
+	for (num1 = 0 ; num1 < 9 ; num1++)
+	{
+	for (num2 = num1 + 1 ; num2 <=9 ; num2++)
+	{
+	putchar(num + '0');
+	if (num1 != 8 && num2 != 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	else
+	{
+	continue;
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
