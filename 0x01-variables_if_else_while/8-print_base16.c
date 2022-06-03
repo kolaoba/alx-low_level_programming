8b04c2dba62ecc309cc6d8d2de1f4e6670951cd0
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-03 07:52:15 -0700

    added 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - program prints numbers in base 16
+ *
+ * Description - program prints single characters
+ * from zero in base 16 using only putchar
+ * Return: Always 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int num;
+	char ch;
+
+	/* your code goes there */
+	for (num = 0 ; num < 10 ; num++)
+	{
+	putchar(num + '0');
+	}
+	for (ch = 'a' ; ch <=f ; ch++)
+	{
+	putchar(ch)
+	}
+	putchar('\n');
+	return (0);
+}
