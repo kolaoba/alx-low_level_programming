f3c1ba0520b0aac9bcdeeb8744fb81e677a22658
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-03 07:08:17 -0700

    added 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - program prints single digit numbers 
+ *
+ * Description - program prints single digit numbers 
+ * from zero in base ten
+ * Return: Always 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int num;
+
+	/* your code goes there */
+	for (num = 0 ; num < 10 ; num++)
+	{
+	putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
