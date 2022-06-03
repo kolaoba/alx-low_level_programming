919debd921478a56f5a9a211f706027159a56df2
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-03 07:58:13 -0700

    added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - program prints single digit numbers
+ *
+ * Description - program prints single digit numbers
+ * from zero in base ten separated by commas
+ * Return: Always 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int num;
+
+	/* your code goes there */
+	for (num = 0 ; num < 10 ; num++)
+	{
+	putchar(num + '0');
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
