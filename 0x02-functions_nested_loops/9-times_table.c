c2e960be31906b523783c94479e157a91589f696
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-07 11:27:50 -0700

    added 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,15 +8,23 @@ void times_table(void)
 {
 	int m = 0;
 	int n;
+	int product;
 
 	while (m < 10)
 	{
-		for (n = 0; n < 10; n++)
+		_putchar('0');
+
+		for (n = 1; n < 10; n++)
 		{
-			_putchar(m * n + '0');
 			_putchar(',');
 			_putchar(' ');
-			_putchar(' ');
+
+			product = m * n;
+
+			if (product < 10)
+				_putchar(' ');
+			else
+				_putchar((product / 10) + '0');
 		}
 		_putchar('\n');
 		m++;
