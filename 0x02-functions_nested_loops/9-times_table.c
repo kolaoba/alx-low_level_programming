2b47e37cd7587161e225d2649320f6d65bdbaf35
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-07 11:12:24 -0700

    added 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,9 @@ void times_table(void)
 		for (n = 0; n < 10; n++)
 		{
 			_putchar(m * n + '0');
-			_putchar(',  ');
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
 		}
 		_putchar('\n');
 		m++;
