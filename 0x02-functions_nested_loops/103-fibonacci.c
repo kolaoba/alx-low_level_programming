483df66c2efda98efc6e430046608aa28f18f711
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-07 14:32:56 -0700

    added 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - program prints the sum of even-valued fibonacci terms less than 4000000
+ * 
+ * Return: Always 0
+ */
+int main(void)
+{
+	int count;
+	unsigned long fib0 = 0, fib1 = 1, next_term, sum = 0;
+
+	for (count = 0; count < 50; count++)
+	{
+		next_term = fib0 + fib1;
+
+		if ((next_term % 2 == 0) || next_term < 4000000)
+		{
+			sum += next_term;
+		}
+
+		fib0 = fib1;
+		fib1 = next_term;
+
+
+
+	}
+	return (0);
+}
