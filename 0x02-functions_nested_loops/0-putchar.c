19b0876b837aab780485b00f17e73a9509a95300
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-07 08:40:38 -0700

    added 0-putchar.c script

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - program prints _putchar to stdout
+ *
+ * Description - program prints _putchar to stdout followed by
+ * a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return(0);
+}
