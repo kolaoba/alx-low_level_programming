8123750eb4d5da78860b46b7664693288bddd23f
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-07 09:51:38 -0700

    5-sign.c added

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,12 +8,16 @@
 int print_sign(int n)
 {
 	if (n < 0)
+	{
 		_putchar('+');
 		return (1);
-	else if
+	} else if
+	{
 		_putchar('0');
 		return (0);
-	else
+	} else
+	{
 		_putchar('-');
 		return (-1);
+	}
 }
