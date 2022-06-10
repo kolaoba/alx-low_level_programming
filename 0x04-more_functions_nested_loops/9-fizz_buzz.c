327bb9eb4fe5d0cf474383a04d3ade45d7fc72dc
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-10 14:56:34 -0700

    Added 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - program prints numbers from 1 to 100 without 3, 5 
+ * or multiples of both. Replace 3 with fizz and 5 with buzz
+ * followed by a new line
+ * 
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3) == 0 && (num % 5) == 0)
+			printf("FizzBuzz");
+		else if (num % 3 == 0)
+			printf("Fizz");
+		else if (num % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", num);
+		if (num == 100)
+			continue;
+		printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
