8414f3e9adcf6b6b174b194744e3fbe68a6c507b
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-10 07:56:53 -0700

    added 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
  * print_numbers - program prints numbers from 0 to 9
- *
- * Return: Always 0
+ * followed by a new line
  */
 void print_numbers(void)
 {
@@ -13,6 +12,4 @@ void print_numbers(void)
 		_putchar(i);
 	}
 	_putchar('\n');
-
-	return (0);
 }
