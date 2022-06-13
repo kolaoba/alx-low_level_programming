1c9e7764ba15b40d8a5845f4efa0cbc9fbe668ba
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-13 12:13:05 -0700

    added 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - program reverses a string.
+ * @s: input string
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+	int index = 0;
+	char swp;
+
+	while (s[index++])
+		length++;
+
+	for (index = length - 1; index >= length / 2; index--)
+		tmp = s[index];
+		s[index] = s[length - index - 1];
+		s[length - index - 1] = tmp;
+
+	_putchar('\n');
+}
