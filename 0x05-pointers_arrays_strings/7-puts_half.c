1ea5fced0a59f7c7af785e91f539baf939a51a78
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-13 15:46:47 -0700

    added 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts_half - program prints half of a string.
+ * @str: input string
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int index = 0;
+
+	while (s[index++])
+		length++;
+	half_length = length / 2;
+
+	while (*(s + half_length) != '\0')
+	       s++;
+	printf("%s", s);
+
+	printf("\n");	
+}
