57f4faf5c040e54f395e54b5fd130cb3acc7211e
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-13 10:17:32 -0700

    added 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * int_strlen - program returns the length of a string
+ * @s: input string
+ *
+ * Return: length of string s
+ */
+void int_strlen(char *s)
+{
+	int length = 0;
+
+	while (*s)
+		length++;
+		*s++;
+	return (length);
+}
