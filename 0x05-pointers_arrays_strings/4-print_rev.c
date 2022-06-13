f83b1060b8a03c5dd568d8f2237a24f0b2903294
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-13 11:18:01 -0700

    Added 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - program prints a string in reverse.
+ * @s: input string
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+	int index;
+
+	while (s[index++])
+		length++;
+
+	for (index = length - 1; index >= 0; index--)
+		_putchar(s[index]);
+
+	_putchar('\n');
+}
