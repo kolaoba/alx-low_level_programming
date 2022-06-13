c620219bfde6cd9bbad9beee2297aed49eb261fb
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-13 15:05:02 -0700

    added 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts2 - program prints every other character of a string
+ * @str: input string
+ */
+void puts2(char *str)
+{
+	int length = 0, index = 0;
+	while (*str[index]++)
+		length++;
+
+	for (index = 0; index <= length; index++)
+	{
+		if (index % 2 == 0)
+			_putchar(str[index]);
+	}
+	_putchar('\n');
+}
