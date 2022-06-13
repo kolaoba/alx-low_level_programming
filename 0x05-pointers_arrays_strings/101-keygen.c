6f610ffbf90bc7631b421322f0089d84622c49d5
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-13 16:18:41 -0700

    added 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int pass[100];
+	int iindex, count, s;
+
+	count = 0;
+
+	srand(time(NULL));
+
+	for (index = 0; index < 100; index++)
+	{
+		pass[index] = rand() % 55;
+		count += (pass[index] + '0');
+		putchar(pass[index] + '0');
+		if ((3672 - count) - '0' < 55)
+		{
+			s = 3672 - count - '0';
+			count += s;
+			putchar(s + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
