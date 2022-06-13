76fe914a1075ade01a6bfb4c2e0c456948549871
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-13 09:51:11 -0700

    added 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98 - program takes a pointer to n int and updates
  *               the value it points to 98.
- * @*n: pointer to be changed
+ * @n: pointer to be changed
  *
  * Return: Always 0.
  */
