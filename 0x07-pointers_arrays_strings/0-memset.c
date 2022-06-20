fcccc3be664fb3194979bd5bf8774bf5f3c91b14
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-06-20 09:19:13 -0700

    added 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,10 +8,10 @@
  *
  * Return: pointer to the filled memory area @s.
  */
-char *_memset(char *s, char c, unsigned int n)
+char *_memset(char *s, char b unsigned int n)
 {
 	unsigned int index;
-	unsigned char *mem = s, value = c;
+	unsigned char *mem = s, value = b;
 
 	for (index = 0; index < n; index++)
 		mem[index] = value;
