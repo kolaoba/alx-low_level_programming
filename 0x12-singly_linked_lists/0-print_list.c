ac879407bc17cf6473d4011630534b92a29c93c0
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-07-14 15:55:51 -0700

    fixed whitespace in 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,11 +2,10 @@
 #include <stdio.h>
 
 /**
- * print_list - prints all the elements of 
- * a list_t list
- * @h: the list_t list
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
  *
- * Return: number of nodes in h
+ * Return: The number of nodes in h.
  */
 
 size_t print_list(const list_t *h)
@@ -17,6 +16,7 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
+
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
