a221bf94be32b995cfe24aeace0483f2052931c5
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-07-19 13:36:58 -0700

    fixed 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,7 +8,7 @@
  *
  * Return: the address of the new element, or NULL if it failed
  */
-list_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
