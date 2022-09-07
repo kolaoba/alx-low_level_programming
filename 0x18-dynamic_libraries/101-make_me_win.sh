956ba87fdfbf61aeb27c462e7944c77842195e36
Kolapo Obajuluwa <kolaobajuluwa@gmail.com>
2022-09-07 12:22:53 -0700

    fixed 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/libtest.so https://github.com/kolaoba/alx-low_level_programming/blob/main/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD=/tmp/libtest.so
+wget -P /tmp https://github.com/lroudge/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libmask.so
+export LD_PRELOAD=/tmp/libmask.so
